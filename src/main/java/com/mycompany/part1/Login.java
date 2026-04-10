e64d519a4c9302a361025be5948f14d290995ec8
RubyChandiwana1 <st10451972@imconnect.edu.za>
2026-04-10 15:26:29 +0200

    public class Login

--- src/main/java/com/mycompany/part1/Login.java
+++ src/main/java/com/mycompany/part1/Login.java
@@ -1,2 +1,8 @@
 import java.util.Objects;
 import java.util.regex.Pattern;
+
+public class Login {
+    private static final Pattern PASSWORD_HAS_UPPER = Pattern.compile("[A-Z]");
+    private static final Pattern PASSWORD_HAS_DIGIT = Pattern.compile("\\d");
+    private static final Pattern PASSWORD_HAS_SPECIAL = Pattern.compile("[^a-zA-Z0-9]");
+
